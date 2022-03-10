736a47cf3d8ff9c5a4f76ef86c600a4065b03a31
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-03-09 22:04:59 -0500

    created an array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("Hello");
+
+//Create array with 26 elements. Each element should be a string consisting of one letter of the alphabet. /*
+
+let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"];
+
+console.log(alphabet);
+
+// created a variable called "letter".
+
+let letter = null;
+
+// create a function that will assign the value of "letter" to a randomly selected element of alphabet array.
