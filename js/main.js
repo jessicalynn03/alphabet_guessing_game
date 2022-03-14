13959db237b48eedd3c8650f7ea71bf318d0e84a
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-03-14 03:57:37 -0400

    game is fully functioning

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 const submitGuess = document.getElementById("submit-guess")
 submitGuess.disabled = true;
 
-//Create array with 26 elements. Each element should be a string consisting of one letter of the alphabet. /*
 
 // Timer function
-
 // let time = 0;
 let seconds = 61;
 
@@ -12,16 +10,18 @@ let updateCountdown;
 
 const countDown = () => {
     updateCountdown = setInterval(function(){
-       if(seconds > 0){
-           seconds--
-           timerNum.innerText = `${seconds}s`;
-           if(seconds === 0){
-               gameOver();
-           }
-       }
+        if(seconds > 0){
+            seconds--
+            timerNum.innerText = `${seconds}s`;
+            if(seconds === 0){
+                gameOver();
+                timerNum.innerText = `Time Out! Computer wins`;
+            }
+        }
     }, 1000)
 }
 
+//Create array with 26 elements. Each element should be a string consisting of one letter of the alphabet. /*
 let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"];
 
 console.log(alphabet);
@@ -36,13 +36,18 @@ let letter = null;
 // randomly choose a letter from the array and assign it to the value letter.
 const randomLetters = () =>{
     const randomIndex = Math.floor(Math.random() * alphabet.length);
+    // assigning whatever is in that array to letter.
     letter = alphabet[randomIndex];
     console.log(letter);
     // console.log(alphabet.splice(randomIndex, 1));
+    // removing from the array the letter that is chosen by computer
     newAlphabet = alphabet.splice(randomIndex, 1);
+    // lettersChosen();
     console.log(newAlphabet);
     console.log(alphabet);
 }
+
+
 // randomLetters()
 
 // create a variable called count and initialize it to zero, then console.log it. 
@@ -93,9 +98,27 @@ const userSubmit = (event) =>{
     // timerNum.innerText = `0s`;
     letterChosen.innerText = `${letter}`;
     // countDown();
+    lettersChosen();
+    lettersChosenReset();
     randomLetters();
 } 
 
+let list;
+let compLetterList = document.getElementById("comp-letter-list");
+
+const lettersChosen = () => {
+    list = document.createElement("li");
+    list.innerHTML = letter;
+    compLetterList.appendChild(list);
+    // document.getElementById("comp-letter-list").appendChild(list);
+}
+let listId = document.getElementById("chosen-letters");
+
+const removeList = () => {
+    // const liEl = document.querySelector("li");
+    compLetterList.remove(list);
+    // document.getElementById("comp-letter-list").remove(list);
+}
 
 // wrote an HTML code that will limit the number of characters in the input field to one character. (line 32)
 
@@ -111,10 +134,10 @@ let compNum = 0; // create var for computer score number and set to 0
 const compareLetters = () => {
     // if user guesses correct letter, add one point to their score
     if(userGuess === letter) {
-        if(userNum < 2) {
+        if(userNum < 12) {
             userNum++;
             userScore.innerText = userNum;
-            if(userNum === 2) {
+            if(userNum === 12) {
                 //game over function
                 clearInterval(updateCountdown);
                 gameOver();
@@ -125,10 +148,10 @@ const compareLetters = () => {
         updateMsg.innerText = `You're Right!`;
         //if user guesses incorrectly, add one point to computer's score
     } else if(userGuess !== letter) {
-        if(compNum < 2) {
+        if(compNum < 12) {
             compNum++
             compScore.innerText = compNum;
-            if(compNum === 2) {
+            if(compNum === 12) {
                 //game over function
                 clearInterval(updateCountdown);
                 gameOver();
@@ -155,12 +178,18 @@ const startGame = () =>{
     countDown();
     randomLetters();
     submitGuess.disabled = false;
+    // lettersChosen();
+    startBtn.removeEventListener("click", startGame);
 }
 startBtn.addEventListener("click", startGame);
 
 
 // Do not have to re-initialize variables. to change an element use innerText or innerContent
 const gameReset = () =>{
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"];
+    // lettersChosen();
+    // const liEl = document.querySelectorAll("li");
+    // const listId = document.getElementById("chosen-letters");
     seconds = 61;
     timerNum.innerText = `60s`;
     clearInterval(updateCountdown);
@@ -173,6 +202,26 @@ const gameReset = () =>{
     gameForm.reset();
     updateMsg.innerText = `Ready to Play!`;
     submitGuess.disabled = true;
+    removeList();
+    compLetterList.appendChild(list);
+    startBtn.addEventListener("click", startGame);
+
+    let ulEl = document.createElement("ul");
+    ulEl.setAttribute("id", "comp-letter-list");
+    chosenLetters.appendChild(ulEl);
+    // list = document.createElement("li");
+    // list.innerHTML = letter;
+    // document.getElementById("comp-letter-list").appendChild(list);
+
+    // lettersChosenReset();
+    // list = document.createElement("li");
+    // list.innerHTML = letter;
+}
+
+const lettersChosenReset = () => { 
+    list = document.createElement("li");
+    list.innerHTML = letter;
+    document.getElementById("comp-letter-list").appendChild(list);
 }
 
 resetBtn.addEventListener("click", gameReset);
